50fb0d3ef1c1b46925d51fa1e70cff98d34ed770
Md Firoj Kabir <firoj.kabir65@gmail.com>
2023-11-09 17:38:25 +0100

    Add routes, remove codes from App component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgToastModule } from 'ng-angular-popup';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostsComponent, StudentsComponent],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
     NgToastModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
